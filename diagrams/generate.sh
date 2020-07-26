51ce4345bb8ffcad17db68f48f345bde88f7feba
Cassandra Fridkin <cass@swag.lgbt>
2020-07-26 00:26:05 -0400

    fix diagram scripting

--- diagrams/generate.sh
+++ diagrams/generate.sh
@@ -1,5 +1,17 @@
-for diagram in diagrams/*.mmd
+CHANGED_DIAGRAMS=""
+for diagram in $(git status --porcelain | grep mmd | cut -d' ' -f3)
 do
   OUTFILE=$(echo $diagram | cut -d'.' -f1).png
   yarn mmdc -i $diagram -o $OUTFILE -t neutral
+  CHANGED_DIAGRAMS="$CHANGED_DIAGRAMS\n$OUTFILE"
 done
+
+if [-z $CHANGED_DIAGRAMS]
+then
+  exit 0
+fi
+
+echo "Created or overwrote the following files:"
+printf "$CHANGED_DIAGRAMS\n\n"
+
+git add diagrams
